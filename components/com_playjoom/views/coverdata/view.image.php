ab5748356083378f5305b2e2e9455e9c94767cc0
Torsten Freyda <torsten@freyda.de>
2016-02-20 16:54:06 +0100

    Add new view file with type image for ajax cover function.

---
+++ components/com_playjoom/views/coverdata/view.image.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package     PlayJoom.Site
+ * @subpackage  com_playjoom
+ *
+ * @copyright Copyright (C) 2010-2016 by www.playjoom.org
+ * @license http://www.playjoom.org/en/about/licenses/gnu-general-public-license.html
+ */
+
+defined('_JEXEC') or die('Restricted access');
+
+// import Joomla view library
+jimport('joomla.application.component.view');
+
+/**
+ * HTML View class for the PlayJoom Component
+ */
+class PlayJoomViewCoverdata extends JViewLegacy {
+
+	// Overwriting JView display method
+	function display($tpl = null) {
+
+		// Get data from the model
+		$this->item = $this->get('Item');
+		echo $this->item->data;
+	}
+}
